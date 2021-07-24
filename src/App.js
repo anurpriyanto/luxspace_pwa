7d12cb3e689e9b42cfe5798ebff635a72de01ee4
DESKTOP-6TOQ7UM\user <anur.priyanto@logol.co.id>
2021-07-25 00:04:17 +0700

    sampai play yt video

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Browse from './components/Browse';
+import Arrived from './components/Arrived';
+import Clients from './components/Clients';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
     <Header/>
     <Hero/>
     <Browse/>
+    <Arrived/>
+    <Clients/>
+    <AsideMenu/>
+    <Footer/>
     </>
   );
 }
